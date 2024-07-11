f2fe2f2b666affd163e24109569011adc276e1c2
Shovon Chowdhury <88574002+svn-111@users.noreply.github.com>
2024-07-11 11:56:35 +0600

    addEnterButton and change color

--- script.js
+++ script.js
@@ -1,23 +1,91 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+const submitButton = document.getElementById('addButton');
 
-function addTask()
-{
+// function addTaskPointer()
+// {
+//     if(inputBox.value ===''){
+//         alert("You must write something");
+//     }
+//     else
+//     {
+//         let li = document.createElement("li");
+//         li.innerHTML = inputBox.value;
+//         listContainer.appendChild(li);
+//         let span = document.createElement("span");
+//         span.innerHTML = "\u00d7";
+//         li.appendChild(span);
+//     }
+//     inputBox.value="";
+//     saveData();
+// }
+// function addTaskPress()
+// {
+//     console.log("Hi");
+//     if(inputBox.value ===''){
+//         alert("You must write something");
+//     }
+//     else
+//     {
+//         inputBox.addEventListener("keypress", function(event) {
+//             // If the user presses the "Enter" key on the keyboard
+//             if (event.key === "Enter") {
+//                 let li = document.createElement("li");
+//                 li.innerHTML = inputBox.value;
+//                 listContainer.appendChild(li);
+//             //   document.getElementById("myBtn").click();
+//             }
+//           });
+//     }
+//     inputBox.value="";
+//     saveData();
+// }
+function handleKeyPress(event) {
+    // Check if the key pressed is Enter (key code 13)
+    if (event.keyCode === 13) {
+      // Trigger the button click
+      if(inputBox.value ===''){
+                alert("You must write something");
+            }
+            else
+            {
+                let li = document.createElement("li");
+                li.innerHTML = inputBox.value;
+                listContainer.appendChild(li);
+                let span = document.createElement("span");
+                span.innerHTML = "\u00d7";
+                li.appendChild(span);
+                saveData();
+            }
+            
+            inputBox.value="";
+    }
+    
+  }
+  
+function handleClick(event) {
+    
     if(inputBox.value ===''){
-        alert("You must write something");
-    }
-    else
-    {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value="";
-    saveData();
-}
+              alert("You must write something");
+          }
+          else
+          {
+              let li = document.createElement("li");
+              li.innerHTML = inputBox.value;
+              listContainer.appendChild(li);
+              let span = document.createElement("span");
+              span.innerHTML = "\u00d7";
+              li.appendChild(span);
+              saveData();
+          }
+          inputBox.value="";
+            
+          
+  }
+submitButton.addEventListener('click', handleClick);
+inputBox.addEventListener('keypress', handleKeyPress);
+//document.addEventListener('keypress', handleKeyPress);
+
 listContainer.addEventListener("click",function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
